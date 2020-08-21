b268853a8b01056da6eb8bd6e656f248da81a6ef
Wjinlei <1976883731@qq.com>
2020-08-22 00:33:31 +0800

    优化

--- config.h
+++ config.h
@@ -1,20 +1,17 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-    /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"",		"dwm.getNetTraf.sh",		1,			16},
-	{"",		"dwm.getMusic.sh",			0,			11},
-	{"",		"dwm.getMail.sh",			0,			12},
-    {"",		"dwm.getCpu.sh",			10,			13},
-    {"",		"dwm.getMemory.sh",		    10,			14},
-    {"",		"dwm.getDisk.sh",			10,			15},
-	{"",		"dwm.getVolume.sh",			0,			10},
-	{"",		"dwm.getBattery.sh",		5,			3},
-    {"",		"dwm.getWeather.sh",		18000,		5},
-	{"",		"dwm.getClock.sh",			60,			1},
-    {"",		"dwm.getMoonPhase.sh",		18000,		17},
-	{"",		"dwm.getInternet.sh",		5,			4},
-	{"",		"dwm.getPackages.sh",		0,			8},
+    {""   , "dwm.getNetTraf.sh"   , 1     , 16} ,
+    {""   , "dwm.getMusic.sh"     , 0     , 11} ,
+    {""   , "dwm.getMail.sh"      , 0     , 12} ,
+    {""   , "dwm.getCpu.sh"       , 10    , 13} ,
+    {""   , "dwm.getMemory.sh"    , 10    , 14} ,
+    {""   , "dwm.getDisk.sh"      , 10    , 15} ,
+    {""   , "dwm.getVolume.sh"    , 0     , 10} ,
+    {""   , "dwm.getBattery.sh"   , 5     , 3}  ,
+    //{"" , "dwm.getWeather.sh"   , 18000 , 5}  ,
+    {""   , "dwm.getClock.sh"     , 60    , 1}  ,
+    {""   , "dwm.getMoonPhase.sh" , 18000 , 17} ,
+    {""   , "dwm.getInternet.sh"  , 5     , 4}  ,
+    {""   , "dwm.getPackages.sh"  , 0     , 8}  ,
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char *delim = ",";
