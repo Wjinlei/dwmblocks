92661cab303d1889f642fd1d141cd946ae176a16
Wjinlei <1976883731@qq.com>
2021-01-29 23:21:51 +0800

    update

--- config.h
+++ config.h
@@ -1,11 +1,10 @@
 static const Block blocks[] = {
     {""   , "dwm.getNetTraf.sh"   , 1     , 16} ,
-    {""   , "dwm.getMusic.sh"     , 0     , 11} ,
     {""   , "dwm.getMail.sh"      , 0     , 12} ,
     {""   , "dwm.getCpu.sh"       , 10    , 13} ,
     //{""   , "rainbarf.sh"         , 1     , 18} ,
-    //{""   , "dwm.getMemory.sh"    , 10    , 14} ,
-    //{""   , "dwm.getDisk.sh"      , 10    , 15} ,
+    {""   , "dwm.getMemory.sh"    , 10    , 14} ,
+    {""   , "dwm.getDisk.sh"      , 10    , 15} ,
     {""   , "dwm.getVolume.sh"    , 0     , 10} ,
     {""   , "dwm.getBattery.sh"   , 5     , 3}  ,
     //{""   , "dwm.getWeather.sh"   , 18000 , 5}  ,
@@ -13,6 +12,8 @@ static const Block blocks[] = {
     //{""   , "dwm.getMoonPhase.sh" , 18000 , 17} ,
     {""   , "dwm.getInternet.sh"  , 5     , 4}  ,
     {""   , "dwm.getPackages.sh"  , 0     , 8}  ,
+    {"🎵"   , "dwm.getMusic.sh"     , 0     , 11} ,
+    {"🇨🇳"   , ""     , 0     , 0} ,
 };
 
 static char *delim = ",";
